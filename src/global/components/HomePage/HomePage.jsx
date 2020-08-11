32028cff093d4bea3a764ce9f7ab2d0ef13ee7a5
mark2357 <marklenton42@gmail.com>
2020-08-11 20:27:31 +1000

    update to version 0.0.6 bugfixes and changes to homepage

--- src/global/components/HomePage/HomePage.jsx
+++ src/global/components/HomePage/HomePage.jsx
@@ -18,7 +18,7 @@ const HomePage = () => {
                     lg={{ size: 6, offset: 3 }}
                 >
                     <Card body className ='homepage-card' inverse color='dark'>
-                        <div>This Website is a showcase of various frontend webpage examples</div>
+                        <div>This Website is a showcase of various frontend webpage examples.</div>
                         <div>This webpage was created using a combination of the following systems:</div>
                         <ul>
                             <li>React</li>
@@ -29,17 +29,21 @@ const HomePage = () => {
                             As this webpage doesn&apos;t have any backend
                             as it&apos;s only contains frontend examples
                             some systems use workarounds to implement
-                            functionality e.g.the login system
+                            functionality e.g.the login system.
                         </div>
                     </Card>
                     <Card body className ='homepage-card mt-5' inverse color='dark'>
                         <CardTitle>
-                        <Link to='/login-example'>Login Example</Link>
+                        <Link to='/login-example/register'>Login Example</Link>
                         </CardTitle>
-                        <div>contains functionality for logging in, registering and resetting your password</div>
+                        <div>
+                            Contains functionality for logging in, registering and resetting your password
+                            (resetting your password is to fully functional as you don&apos;t receive an email)
+                        </div>
                         <div>
                             Please note that as this is a front end example this website stores registration data in local storage.
                             This is a design patten I would <b>NOT</b> use in a real world solution.
+                            It is only to allow this frontend example to have more functionality.
                         </div>
                     </Card>
                 </Col>
